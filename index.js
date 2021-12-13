8ff61cee0b6ead493e60dce58ee92c24e65f4f74
Doğa Budak <dogabudak@gmail.com>
2021-12-13 09:55:15 +0100

    Made a small refactor and modernization

--- index.js
+++ index.js
@@ -2,29 +2,27 @@
  * Created by doga on 05/12/2016.
  */
 
-var verification = require('./lib/verification-factory'),
-    nano = require('nanomsg'),
-    config= require('./resources/config'),
-    rep = nano.socket('rep');
-
+const verification = require('./lib/verification-factory');
+const nano = require('nanomsg');
+const config = require('./resources/config');
+const rep = nano.socket('rep');
 
 
 rep.bind(config.nanomsg.addr);
 
 rep.on('data', function (buf) {
-    var start = Date.now();
-    var req = buf.toString().split(' ');
+    const req = buf.toString().split(' ');
     if (req.length !== 2) {
         rep.send("false")
         return
     }
-    verification(req[0], req[1], function (err, answer) {
+    verification(req[0], req[1], (err, answer) => {
         if (err) {
             rep.send("false");
             return
         }
-        var auth = JSON.parse(answer);
-        var result = auth.authenticated === true ? "true" : "false";
+        const auth = JSON.parse(answer);
+        const result = auth.authenticated === true ? "true" : "false";
         rep.send(result)
 
     });
